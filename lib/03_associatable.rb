74f1d633bda0406bb6fdaed16f0ebf76290b351b
Peter Fonseca <peter.nfonseca@gmail.com>
2016-05-28 09:30:47 -0700

    more associations

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -43,17 +43,25 @@ module Associatable
     define_method(name) do
       options = self.class.assoc_options[name]
 
-      primary_key_val = self.send(options.foreign_key)
-      options.model_class.where(options.primary_key => primary_key_val)
+      key_val = self.send(options.foreign_key)
+      options.model_class.where(options.primary_key => key_val).first
     end
   end
 
   def has_many(name, options = {})
-    # ...
+    self.assoc_options[name] = HasManyOptions.new(name, self.class.name, options)
+
+    define_method(name) do
+      options = self.class.assoc_options[name]
+
+      key_val = self.send(options.primary_key)
+      options.model_class.where(options.foreign_key => key_val)
+    end
   end
 
   def assoc_options
-    # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
+    @assoc_options
   end
 end
 
